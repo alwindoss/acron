e93f9b9009e81f1a550171bd9aa858448cce7fae
Alwin Doss <alwindoss84@gmail.com>
2017-06-10 22:02:38 +0530

    Writing the db to HOME/.acron/bolt.db

--- db/db.go
+++ db/db.go
@@ -4,12 +4,23 @@ import (
 	"github.com/boltdb/bolt"
 	"log"
 	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"os"
 )
 
 var acronBucket = []byte("acron")
 
 func createDB() *bolt.DB {
-	db, err := bolt.Open("bolt.db", 0644, nil)
+	home, err := homedir.Dir()
+	path := home + "/.acron"
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.Mkdir(path, 755)
+	}
+	fmt.Println("Path: ", path)
+	db, err := bolt.Open(path + "/bolt.db", 0644, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
